8ad3add69fb2610acc5e11e69ab63d3b466e90dd
acreilly <acreilly3@gmail.com>
2014-10-24 13:17:01 -0400

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,27 +9,3 @@
 
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
-
-
-use_codes = {
-  "A" => "Federal government",
-  "B" => "State government",
-  "C" => "Tribe/Status Indian/Indian Band",
-  "D" => "Foreign diplomat",
-  "E" => "Charitable or benevolent organization",
-  "F" => "Religious or educational organization",
-  "G" => "Resale",
-  "H" => "Commercial agricultural production",
-  "I" => "Industrial production/manufacturer",
-  "J" => "Direct pay permit",
-  "K" => "Direct mail",
-  "L" => "Other",
-  "N" => "Local government",
-  "P" => "Commercial aquaculture (Canada only)",
-  "Q" => "Commercial fishery (Canada only)",
-  "R" => "Non-resident (Canada only)"
-}
-
-use_codes.each do |key, value|
-  Spree::AvalaraUseCodeItem.create(use_code: key, use_code_description: value)
-end
