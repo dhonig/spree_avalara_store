52720a07c95b8af61e4a6df88c55779631c297bb
acreilly <acreilly3@gmail.com>
2014-10-24 13:26:35 -0400

    removed endpoint

--- config/initializers/avalara.rb
+++ config/initializers/avalara.rb
@@ -1,5 +1,4 @@
 Avalara.configure do |config|
   config.username = ENV['AVALARA_USERNAME']
   config.password = ENV['AVALARA_PASSWORD']
-  # config.endpoint = "https://developement.avalara.net"
 end
